6231b8779cc96d8ececef77e12366b1bb89e36b2
Andrey <andrey@Air-Andrey.netis.cc>
2024-08-21 21:55:36 +0400

    lesson2-2

---
+++ 2-2/app.js
@@ -0,0 +1,15 @@
+// Домашнее задание 2.3
+
+const titleProject = 'Рассчет стоимости';
+const screensValue = ['шаблонные', 'с уникальным дизайном', 'с анимациями'];
+const screensPrice = 100;
+const percentage = 15;
+const fullPrice = 1500;
+const responsive = true;
+
+console.log(titleProject);
+console.log(screensValue);
+console.log(screensPrice);
+console.log(percentage);
+console.log(fullPrice);
+console.log(responsive);
